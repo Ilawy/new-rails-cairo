8a0b630d7a015e02cf20bd0cfc1c1316451ccf37
ilawy <next.mohammed.amr@gmail.com>
2025-05-17 14:50:35 +0300

    feat: add user model validation rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :dob, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, uniqueness: true
+  validates :phone_number, presence: true
 end
