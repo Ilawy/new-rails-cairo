a68c7a02b2a0c8b0ddcf8a53e61b639bc9186e32
ilawy <next.mohammed.amr@gmail.com>
2025-05-17 15:17:05 +0300

    feat: add address field to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_17_110313) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_17_121625) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_17_110313) do
     t.string "phone_number", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address", null: false
     t.index ["email"], name: "unique_emails", unique: true
   end
 end
